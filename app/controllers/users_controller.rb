8db44481641dd55d88f0ca724691f223b33ca333
InoKotaro <114914604+InoKotaro@users.noreply.github.com>
2022-11-21 08:25:12 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+	before_action :is_matching_login_user, only: [:edit, :update]
 
 	def index
+		@user= current_user
 		@users= User.all
 	end
 
@@ -15,12 +17,25 @@ class UsersController < ApplicationController
 
 	def update
 		@user= User.find(params[:id])
-		@user.update(user_params)
-		redirect_to user_path(@user)
+		if @user.update(user_params)
+			flash[:notice]= "You have updated user successfully."
+			redirect_to users_path(@user)
+		else
+			render :edit
+		end
 	end
 
-	def user_params
-    params.require(:user).permit(:name, :profile_image)
-  end
+	private
 
+	def user_params
+    	params.require(:user).permit(:name, :profile_image)
+	end
+
+	def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to books_path
+    end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
